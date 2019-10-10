1817eed94fef401f1629c67d7b031ac2622b04d2
Mazay Jiménez Salinas <mazay.jimenez@gmail.com>
2019-10-09 17:19:39 -0700

    initial .emacs configuration

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (aggressive-indent comment-dwim-2 linum-relative smartparens restclient flycheck-clojure ibuffer-projectile projectile git-auto-commit-mode elisp-slime-nav align-cljlet cider clojure-mode eww-lnum emojify beginend selected goto-chg interaction-log discover-my-major help-fns+ which-key edit-list iedit highlight-symbol visual-regexp hungry-delete easy-kill browse-kill-ring expand-region ace-link avy-zap ace-window avy smex flx-ido ido-vertical-mode ivy-rich counsel zoom deadgrep dired-collapse dired-quick-sort dired-subtree dired-filter dired+ f esh-autosuggest counsel-osx-app yaml-mode use-package spacemacs-theme solarized-theme smart-mode-line rainbow-delimiters py-autopep8 paradox osx-clipboard org-caldav magit-popup magit groovy-mode go-guru go-eldoc go-add-tags flycheck fill-column-indicator exec-path-from-shell elpy ein doom-themes cyberpunk-theme company-go better-defaults all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:foreground "#171717" :weight bold :height 1.5))))
+ '(org-done ((t (:background "#E8E8E8" :foreground "#0E0E0E" :strike-through t :weight bold))))
+ '(org-headline-done ((t (:foreground "#171717" :strike-through t))))
+ '(org-image-actual-width (quote (600)))
+ '(org-level-1 ((t (:foreground "#090909" :weight bold :height 1.3))))
+ '(org-level-2 ((t (:foreground "#090909" :weight normal :height 1.2))))
+ '(org-level-3 ((t (:foreground "#090909" :weight normal :height 1.1))))
+ '(variable-pitch ((t (:family "ETBembo")))))
